5fdb9bfdddb773b66638e5c1457de2f4ac2c74e8
ktitova <ksunchik123@yandex.ru>
2022-04-27 22:26:53 +0300

    isPositiveNumberTest

--- src/test/java/IsPositiveNumberTest.java
+++ src/test/java/IsPositiveNumberTest.java
@@ -6,19 +6,39 @@ public class IsPositiveNumberTest {
 
     @Order(1)
     @Test
-    public void OddIndicesTestHappy() {
+    public void IsPositiveNumberTestHappy() {
         int a = 555;
         boolean expectedResult = true;
 
         IsPositiveNumber isPositiveNumber = new IsPositiveNumber();
         boolean actualResult = isPositiveNumber.isPositiveNumberAlgorithm(a);
 
-        Assertions.assertEquals(expectedResult,actualResult);
-
+        Assertions.assertEquals(expectedResult, actualResult);
     }
 
 
+    @Order(2)
+    @Test
+    public void IsPositiveNumberTestHappyZero() {
+        int a = 0;
+        boolean expectedResult = true;
+
+        IsPositiveNumber isPositiveNumber = new IsPositiveNumber();
+        boolean actualResult = isPositiveNumber.isPositiveNumberAlgorithm(a);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 
 
+    @Order(3)
+    @Test
+    public void IsPositiveNumberTestNeg() {
+        int a = -555;
+        boolean expectedResult = false;
 
+        IsPositiveNumber isPositiveNumber = new IsPositiveNumber();
+        boolean actualResult = isPositiveNumber.isPositiveNumberAlgorithm(a);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }
