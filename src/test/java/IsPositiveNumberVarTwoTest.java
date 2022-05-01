9812e0d1badd666a056244e98adc10089dc18629
ktitova <ksunchik123@yandex.ru>
2022-05-01 19:55:21 +0300

    fix the code

--- src/test/java/IsPositiveNumberVarTwoTest.java
+++ src/test/java/IsPositiveNumberVarTwoTest.java
@@ -1,4 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
 public class IsPositiveNumberVarTwoTest {
 
+    @Order(1)
+    @Test
+    public void isPositiveNumberTestHappy() {
+        int a = 555;
+        boolean expectedResult = true;
 
+        IsPositiveNumberVarTwo isPositiveNumberVarTwo = new IsPositiveNumberVarTwo();
+        boolean actualResult = isPositiveNumberVarTwo.isPositiveNumberAlgorithmVarTwo(a);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Order(2)
+    @Test
+    public void isPositiveNumberTestHappyTwo() {
+        int a = -555;
+        boolean expectedResult = false;
+
+        IsPositiveNumberVarTwo isPositiveNumberVarTwo = new IsPositiveNumberVarTwo();
+        boolean actualResult = isPositiveNumberVarTwo.isPositiveNumberAlgorithmVarTwo(a);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }
