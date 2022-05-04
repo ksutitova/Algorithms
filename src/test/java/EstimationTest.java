8f4cb50b46fa82a1d24ef0a690f26c1eaa0836f6
ktitova <ksunchik123@yandex.ru>
2022-05-04 09:49:05 +0300

    estimation Tests

--- src/test/java/EstimationTest.java
+++ src/test/java/EstimationTest.java
@@ -20,7 +20,7 @@ public class EstimationTest {
 
     @Order(1)
     @Test
-    public void testEstimationHappy() {
+    public void testEstimationHappy5() {
         int n = 5;
         String expectedResult = "выдать похвальную грамоту и перевести в следующий класс";
 
@@ -29,5 +29,45 @@ public class EstimationTest {
 
         Assertions.assertEquals(expectedResult, actualResult);
     }
+
+
+    @Order(2)
+    @Test
+    public void testEstimationHappy4() {
+        int n = 4;
+        String expectedResult = "перевести в следующий класс";
+
+        Estimation estimation = new Estimation();
+        String actualResult = estimation.estimationAlgorithm(n);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Order(3)
+    @Test
+    public void testEstimationHappy3() {
+        int n = 3;
+        String expectedResult = "дать задание на лето и перевести в следующий класс";
+
+        Estimation estimation = new Estimation();
+        String actualResult = estimation.estimationAlgorithm(n);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Order(4)
+    @Test
+    public void testEstimationHappy2() {
+        int n = 2;
+        String expectedResult = "вызвать родителей и оставить в текущем классе на второй год";
+
+        Estimation estimation = new Estimation();
+        String actualResult = estimation.estimationAlgorithm(n);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
 
 }
